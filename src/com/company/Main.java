a78ffc569f1a7bbdfd38f4092878ce602ffa51c5
barhom <barhom@knights.ucf.edu>
2021-09-13 22:06:35 -0400

    alko-cop3330-ex03

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 barhom alko
+ */
+
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is the quote? ");
+        String quote = sc.nextLine();
+        System.out.print("Who said it? ");
+        String author = sc.nextLine();
+        System.out.println(author + " says, \""+quote+"\"");
+    }
+}
